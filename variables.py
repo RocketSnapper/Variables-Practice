79e589c766032c2e10b44f8291a675b244632dbb
usjamesmc <usjamesmc@gmail.com>
2025-11-03 13:14:15 -0600

    Completed some practice variable exercises.

---
+++ variables.py
@@ -0,0 +1,14 @@
+age = 34
+print(f'I am {age} years old')
+
+first_name = input('What is your first name?')
+
+last_name = input('What is your last name?')
+
+full_name = first_name + last_name
+
+print(f'My first name is {first_name} and my last name is {last_name}, which means my full name is {full_name}')
+
+farenheit_temp = 75
+celcius_temp = (farenheit_temp - 32) / 1.8
+print(celcius_temp)
